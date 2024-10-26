ad464a80a4836e2560cbaa1c2808b408472677d4
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-10-26 20:49:06 +0700

    30 Reuse Connection

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, //defaul 3306
+    user: process.env.DB_USER, // defaul: empty
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -15,22 +15,11 @@ configViewEngine(app);
 // Khai bao route
 app.use('/', webRoutes);
 
-// Test connection
-// Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307, //defaul 3306
-  user: 'root', // defaul: empty
-  password: '123456',
-  database: 'tienho'
-});
-
 // simple query
 connection.query(
     'SELECT * FROM Users',
     function (err, results, fields) {
-      console.log(">>>>>results= ",results); // results contains rows returned by server
-      console.log(">>>>>fields= ",fields); // fields contains extra meta data about results, if available
+      console.log(">>>>>results= ",results); // results contains rows returned by server console.log(">>>>>fields= ",fields); // fields contains extra meta data about results, if available
     }
 );
 
