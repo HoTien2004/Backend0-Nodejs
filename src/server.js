fb2ec2abfe87191efb97ef45f9a3005beedf21c8
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-11-10 03:26:53 +0700

    CRUD mongodb

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const connection = require("./config/database");
-const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -18,14 +17,6 @@ app.use(express.urlencoded({ extended: true })); // for form data
 
 // Declare route
 app.use("/", webRoutes);
-
-const kittySchema = new mongoose.Schema({
-  name: String,
-});
-
-const Kitten = mongoose.model("Kitten", kittySchema);
-const silence = new Kitten({ name: "Hoi dan it Cat" });
-silence.save();
 
 (async () => {
   await connection();
