595907a97d62de48ffafb4c73987c1b287a43afc
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-11-11 23:30:12 +0700

    PUT & DELETE

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,26 +1,22 @@
 const User = require("../models/user");
 
 const getUsersAPI = async (req, res) => {
-  let results = await User.find();
-  
-  return res.status(200).json(
-    {
-        errorCode: 0,
-        data: results
-    }
-  )
+    let results = await User.find();
+    
+    return res.status(200).json(
+        {
+            errorCode: 0,
+            data: results
+        }
+    )
 };
 
 const postCreateUserAPI = async (req, res) => {
     let email = req.body.email;
-    let name = req.body.myname;
+    let name = req.body.name;
     let city = req.body.city;
   
-    let user = await User.create({
-        email,
-        name,
-        city,
-    });
+    let user = await User.create({email, name, city,});
     
     return res.status(200).json(
         {
@@ -29,8 +25,39 @@ const postCreateUserAPI = async (req, res) => {
         }
     )
 };
+  
+const putUpdateUserAPI = async (req, res) => {
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+    let userId = req.body.userId;
+  
+    let user = await User.updateOne({ _id: userId}, { email: email, name: name, city: city });
+
+    return res.status(200).json(
+        {
+            errorCode: 0,
+            data: user
+        }
+    )
+};
+
+const deleteUserAPI = async (req, res) => {
+    const id = req.body.userId;
+    let results = await User.deleteOne({
+      _id: id
+    })
+    return res.status(200).json(
+        {
+            errorCode: 0,
+            data: results
+        }
+    )
+  };
 
 module.exports = {
     getUsersAPI,
-    postCreateUserAPI
+    postCreateUserAPI,
+    putUpdateUserAPI,
+    deleteUserAPI
 }
