e1dd3ae337d6949a37075ded72a004dff81760cc
QUYẾT TIẾN <152900541+HoTien2004@users.noreply.github.com>
2024-11-18 18:16:23 +0700

    API Get Customers with pagination

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -27,9 +27,15 @@ const createArrayCustomerService = async (arr) => {
     }
 }
 
-const getAllCustomerService = async () => {
+const getAllCustomerService = async (limit, page) => {
     try {
-        let result = await Customer.find({});
+        let result = null;
+        if (limit && page) {
+            let skip = (page - 1) * limit;
+            result = await Customer.find({}).skip(skip).limit(limit).exec();
+        } else {
+            result = await Customer.find({});
+        }
         return result;
     } catch (err) {
         console.log(">>> Check error: ", err.message);
